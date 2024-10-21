338b7b1597cca9d1a096e4a2019ac2fa831dba1b
nagi0629 <haru8629@icloud.com>
2024-10-21 13:26:37 +0900

    10/21 ver1.0

--- TAKOEngine/Rendering/ConstantBuffer.h
+++ TAKOEngine/Rendering/ConstantBuffer.h
@@ -11,10 +11,10 @@ struct CbScene
 	DirectX::XMFLOAT4   camera_position;
 
 	//ライト情報
-	DirectX::XMFLOAT4				ambientLightColor;
-	DirectionalLightData				directionalLightData;
-	PointLightData					pointLightData[PointLightMax];			// 点光源情報
-	SpotLightData					spotLightData[SpotLightMax];			// スポットライト情報
+	DirectX::XMFLOAT4		ambientLightColor;
+	DirectionalLightData	directionalLightData;
+	PointLightData			pointLightData[PointLightMax];			// 点光源情報
+	SpotLightData			spotLightData[SpotLightMax];			// スポットライト情報
 	int						pointLightCount = 0;				// 点光源数
 	int						spotLightCount = 0;				// スポットライト数
 };
