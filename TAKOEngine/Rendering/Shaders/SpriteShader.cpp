1c7c66698ac1c491f5a4c40e62fabba65c1041d6
nagi0629 <131993397+nagi0629@users.noreply.github.com>
2024-10-31 15:15:31 +0900

    修正

--- TAKOEngine/Rendering/Shaders/SpriteShader.cpp
+++ TAKOEngine/Rendering/Shaders/SpriteShader.cpp
@@ -13,14 +13,15 @@
 //**********************************************************************
 SpriteShader::SpriteShader(ID3D11Device* device, const char* vs, const char* ps)
 {
-	// ���̓��C�A�E�g
+	// 入力レイアウト
 	D3D11_INPUT_ELEMENT_DESC inputElementDesc[] =
 	{
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,    0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 		{ "COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,       0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 	};
-	// ���_�V�F�[�_�[
+
+	// 頂点シェーダー
 	GpuResourceUtils::LoadVertexShader(
 		device,
 		vs,
@@ -29,7 +30,8 @@ SpriteShader::SpriteShader(ID3D11Device* device, const char* vs, const char* ps)
 		inputLayout.GetAddressOf(),
 		vertexShader.GetAddressOf()
 	);
-	// �s�N�Z���V�F�[�_�[
+
+	// ピクセルシェーダー
 	GpuResourceUtils::LoadPixelShader(
 		device,
 		ps,
@@ -63,7 +65,7 @@ void SpriteShader::Begin(const RenderContext& rc)
 	rc.deviceContext->OMSetDepthStencilState(rc.renderState->GetDepthStencilState(DepthState::TestAndWrite), 0);
 	rc.deviceContext->RSSetState(rc.renderState->GetRasterizerState(RasterizerState::SolidCullBack));
 
-	// �T���v���X�e�[�g
+	// サンプラステート
 	ID3D11SamplerState* samplerStates[] =
 	{
 		rc.renderState->GetSamplerState(SamplerState::LinearClamp)
