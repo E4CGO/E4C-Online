3bf5c0869a1d916850e1a03bc1632fd3699f95d0
Yujin <103164783+YevgeniiDimoglo@users.noreply.github.com>
2024-11-05 15:40:52 +0900

    Update MapTileManager.cpp

--- Source/Map/MapTileManager.cpp
+++ Source/Map/MapTileManager.cpp
@@ -1,6 +1,6 @@
 #include "MapTileManager.h"
 
-// ƒŒƒCƒLƒƒƒXƒg
+// ãƒ¬ã‚¤ã‚­ãƒ£ã‚¹ãƒˆ
 bool MapTileManager::RayCast(const DirectX::XMFLOAT3& start, const DirectX::XMFLOAT3& end, HitResult& hit, bool camera)
 {
 	if (start == end) return false;
@@ -12,7 +12,7 @@ bool MapTileManager::RayCast(const DirectX::XMFLOAT3& start, const DirectX::XMFL
 		if (item->GetCollider() == nullptr) continue;
 		if (item->GetCollider()->RayCast(start, end, temp) && (temp.distance < hit.distance))
 		{
-			hit = temp; // Å’Z
+			hit = temp; // ÂÃ…â€™Z
 		}
 	}
 	return hit.distance < FLT_MAX;
